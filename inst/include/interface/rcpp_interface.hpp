dbe35584eb747b19e1edd4b766e062116814220a
msupernaw <matthew.supernaw@noaa.gov>
2022-03-22 14:45:58 -0400

    Update rcpp_interface.hpp

--- inst/include/interface/rcpp_interface.hpp
+++ inst/include/interface/rcpp_interface.hpp
@@ -40,7 +40,8 @@ void addData(Rcpp::DataFrame& data){
    std::cout<<"size = "<<data.size()<<"\n";
 Rcpp::DataFrame::iterator it;
 
-    std::cout<<"name = "<<data.names()<<"\n";
+  Rcpp::CharacterVector df_names = data.names();
+  Rcpp::Rcout << "col names: " << df_names << std::endl;
 
 
 
