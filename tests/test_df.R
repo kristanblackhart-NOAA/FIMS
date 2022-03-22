6bdfd32b3f9bb150ffc63fc50168e1fcd8dd9fd7
msupernaw <matthew.supernaw@noaa.gov>
2022-03-22 15:46:26 -0400

    update

--- tests/test_df.R
+++ tests/test_df.R
@@ -1,17 +1,23 @@
-remotes::install_github("NOAA-FIMS/FIMS@feat-dimension-folding")
+# #Maybe don't need this here if move to test that, in
+ remotes::install_github("NOAA-FIMS/FIMS@feat-dimension-folding")
 library(TMB)
-library(Rcpp)
-library(FIMS)
+ library(Rcpp)
+ library(FIMS)
 
-fims<-Rcpp::Module(module = "fims",PACKAGE = "FIMS")
+fims <- Rcpp::Module(module = "fims", PACKAGE = "FIMS")
 
-abundace.data <- data.frame(
+input.data <- data.frame(
+  type = "index", # or something else?
   ages = c (1:5), 
-  years = c(2001,2002,2003,2004,2005),
-  data = c(623.3,515.2,611.0,729.0,843.25), 
-  units = "mt"
+  years = c(2001, 2002, 2003, 2004, 2005),
+  value = c(623.3, 515.2, 611.0, 729.0, 843.25,
+          623.3, 515.2, NA, 729.0, 843.25,
+          623.3, 515.2, 611.0, 729.0, 843.25,
+          623.3, 515.2, 611.0, NA, 843.25,
+          623.3, 515.2, 611.0, 729.0, 843.25), 
+  units = "numbers"
 )
 
-fims$addData(abundace.data)
+fims$addData(input.data)
 
 
