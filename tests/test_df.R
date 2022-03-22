a3f2375029bed935fb5266f0840969d8674e6e9f
msupernaw <matthew.supernaw@noaa.gov>
2022-03-22 14:20:19 -0400

    Update test_df.R

--- tests/test_df.R
+++ tests/test_df.R
@@ -8,7 +8,7 @@ fims<-Rcpp::Module(module = "fims",PACKAGE = "FIMS")
 abundace.data <- data.frame(
   ages = c (1:5), 
   years = c(2001,2002,2003,2004,2005),
-  salary = c(623.3,515.2,611.0,729.0,843.25), 
+  data = c(623.3,515.2,611.0,729.0,843.25), 
   units = "mt"
 )
 
