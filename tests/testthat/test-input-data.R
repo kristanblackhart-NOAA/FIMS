2d43a6a8573b8537f2e6baf6662e9fbd6d9c5ece
Kathryn Doering <kathryn.doering@noaa.gov>
2022-03-24 13:56:37 -0400

    add catch data

--- tests/testthat/test-input-data.R
+++ tests/testthat/test-input-data.R
@@ -1,7 +1,10 @@
+# Description -----
+# Creates a dataframe containing data to pass to FIMS. We want to be able to pass
+# these in so they can contribute correctly to the likelihood.
+# Note we still want to convert this so it has testthat expectations.
 
-#fims <- Rcpp::Module(module = "fims", PACKAGE = "FIMS")
-
-input.data <- data.frame(
+# Input data for trend ------
+input_data_trend <- data.frame(
   # Trend is a type of data, where it can include CPUE, surveys, or environmental covariate
   type = "trend",
   # Name would be the "pretty" name printed on figures and in tables, where name refers to
@@ -20,15 +23,41 @@ input.data <- data.frame(
   SE = 0.01
 )
 
-input.data$type = factor(input.data$type, levels = c("catch","trend","age_comp","length_comp"))
+# catch
+input_data_catch <- data.frame(
+  # Trend is a type of data, where it can include CPUE, surveys, or environmental covariate
+  type = "catch",
+  # Name would be the "pretty" name printed on figures and in tables, where name refers to
+  # the fleet or survey
+  name = "Index_1",
+  ages = c(1:5),
+  years = c(rep(2001, length.out = 5), rep(2002, length.out =  5),
+            rep(2003, length.out = 5), rep(2004, length.out =  5),
+            rep(2005, length.out = 5)),
+  value = c(623.3, 515.2, 611.0, 729.0, 843.25,
+          623.3, 515.2, 611.0, 729.0, 843.25,
+          623.3, 515.2, 611.0, 729.0, 843.25,
+          623.3, 515.2, 611.0, 729.0, 843.25,
+          623.3, 515.2, 611.0, 729.0, 843.25),
+  units = "numbers",
+  SE = 0.01
+)
 
-make.model.data <- function(data)
-{
-    
-}
+# combine the dataframes because we just want one input dataframe
+input_data <- rbind(input_data_trend, input_data_catch)
 
-# fims$hello_fims()
- print(input.data)
-fims$addData(input.data)
+# Code snippets we may or may not need ------
+# May want to make things into factors, but not sure yet
+# input_data$type = factor(input_data$type,
+# levels = c("catch", "trend"," age_comp", "length_comp"))
 
+# in case we want to write a function to make the data frame, we could make a
+# fxn like this (but would move it to the R sub directory)
+# make.model.data <- function(data)
+# {
+#     data$
+# }
 
+# fims$addData(input.data)
+
+#fims <- Rcpp::Module(module = "fims", PACKAGE = "FIMS")
